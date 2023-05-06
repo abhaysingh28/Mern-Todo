e47aff036285bdd8d2248932619491b71dc909e5
अभय सिंह <99662885+abhaysingh28@users.noreply.github.com>
2023-05-06 11:45:06 +0530

    Update database.js

--- api/Config/database.js
+++ api/Config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.databaseconnection = async() => {
     try{
-        mongoose.connect(process.env.MONGO_URL || "mongodb+srv://yash:yash@cluster0.gpgoazj.mongodb.net/test");
+        mongoose.connect(process.env.MONGO_URL || "mongodb+srv://abhay:abhay@cluster0.gpgoazj.mongodb.net/test");
         console.log('Database connected');
     } catch (error) {
         console.log('Database connection failed ');
